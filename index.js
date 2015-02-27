38ac19a72e34f0d5a4521e10cc403d3d1ced86b1
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-27 12:20:11 -0500

    run `update`

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 /*!
  * git-user-name <https://github.com/jonschlinkert/git-user-name>
  *
- * Copyright (c) 2014 Jon Schlinkert
- * Licensed under the MIT License (MIT)
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
-const shell = require('shelljs');
+var shell = require('shelljs');
 
-module.exports = shell.exec('git config --get user.name', {
-  silent: true
-}).output.trim();
+module.exports = function(cmd, options) {
+  if (typeof cmd === 'object') {
+    options = cmd;
+    cmd = null;
+  }
+
+  var cmd = 'git config --get user.name';
+  var opts = options || {};
+  opts.silent = opts.silent || true;
+
+  var res = shell.exec(cmd, opts);
+  return res.output.trim();
+};
