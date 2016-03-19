d05850838196d0330e9c7a05651147a922bea56c
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-19 13:07:16 -0400

    run update

--- test.js
+++ test.js
@@ -10,8 +10,8 @@
 var assert = require('assert');
 var name = require('./');
 
-describe('name', function () {
-  it('should return the name from git config:', function () {
+describe('name', function() {
+  it('should return the name from git config:', function() {
     assert(name() === 'jonschlinkert');
   });
 });
