5a21eb25577dc21fdc11200ad5534c66f8e63593
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-19 13:22:21 -0400

    fix tests

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
 'use strict';
 
 require('mocha');
+var fs = require('fs');
 var assert = require('assert');
+var utils = require('./utils');
 var name = require('./');
 
 describe('name', function() {
-  it('should return the name from git config:', function() {
-    assert.equal(name(), 'jonschlinkert');
+  it('should return the name from git config:', function(cb) {
+    fs.stat(utils.gitconfig, function(err) {
+      if (err) return cb();
+      assert.equal(name(), 'jonschlinkert');
+      cb();
+    });
   });
 });
