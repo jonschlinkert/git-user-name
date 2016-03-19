66dba1647e91d1bf0f2057c4cbda5a9aef2ebb22
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-19 13:15:38 -0400

    improve check

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 /*!
  * git-user-name <https://github.com/jonschlinkert/git-user-name>
  *
- * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Copyright (c) 2014-2016, Jon Schlinkert.
  * Licensed under the MIT License.
  */
 
-var fs = require('fs');
-var path = require('path');
-var gitconfig = require('git-config-path');
-var parse = require('parse-git-config');
-var extend = require('extend-shallow');
+var utils = require('./utils');
 
 module.exports = function(options) {
-  options = extend({cwd: '/', path: gitconfig}, options);
-  var config = parse.sync(options);
-  if (typeof config === 'object' && config.hasOwnProperty('user')) {
-    return config.user && config.user.name;
+  options = utils.extend({cwd: '/', path: utils.gitconfig}, options);
+  var config = utils.parse.sync(options);
+  if (!utils.isObject(config) || !utils.isObject(config.user)) {
+    return null;
   }
-  return null;
+  return config.user.name;
 };
